f06d7e5eec8fba6a47f2742bb353dc90b2463b48
Aleksandr Yulin <aleksandr.s.yulin@gmail.com>
2019-02-07 16:45:38 +0300

    added phpunit

---
+++ test/ConfigurationTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the Common package, a StreamCommon open software project.
+ *
+ * @copyright (c) 2019 StreamCommon Team.
+ * @see https://github.com/streamcommon/excess-configuration
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace StreamCommon\Test\Excess\Configuration;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ConfigurationTest
+ *
+ * @package StreamCommon\Test\Excess\Configuration
+ */
+class ConfigurationTest extends TestCase
+{
+
+}
